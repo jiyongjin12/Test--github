66c78d9dbf8faf98b40c318f8b0b339834a6d205
jiyongjin <l1110hj@gmail.com>
2023-04-19 23:55:48 +0900

    test
    he

--- Assets/scripts/Weapons/AimWeaponEvent.cs
+++ Assets/scripts/Weapons/AimWeaponEvent.cs
@@ -5,5 +5,18 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class AimWeaponEvent : MonoBehaviour
 {
-    //15:00
+    public event Action<AimWeaponEvent, AimWeaponEventArgs> OnWeaponAim;
+    
 }
+
+
+
+public class AimWeaponEventArgs : EventArgs
+{
+    public AimDirection aimDirection;
+    public float aimAngle;
+    public float weaponAimAngle;
+    public Vector3 weaponAimDirectionVector;
+}
+
+//16:00
