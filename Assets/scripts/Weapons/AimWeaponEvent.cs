608e4e0dc2c15bbef8f5e65cc345e38a71596aa8
jiyongjin <l1110hj@gmail.com>
2023-04-24 00:11:26 +0900

    ..
    .

--- Assets/scripts/Weapons/AimWeaponEvent.cs
+++ Assets/scripts/Weapons/AimWeaponEvent.cs
@@ -1,22 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 [DisallowMultipleComponent]
 public class AimWeaponEvent : MonoBehaviour
 {
     public event Action<AimWeaponEvent, AimWeaponEventArgs> OnWeaponAim;
-    
+
+    public void CallAimWeaponEvent(AimDirection aimDirection, float aimAnlg, float weaponAimAngle, Vector3 weaponAimDirectionVector)
+    {
+        OnWeaponAim?.Invoke(this, new AimWeaponEventArgs()
+        {
+            aimDirection = aimDirection,
+            aimAnlge = aimAnlg,
+            weaponAimAngle = weaponAimAngle,
+            weaponAimDirectionVector = weaponAimDirectionVector
+        });
+    }
 }
-
-
 
 public class AimWeaponEventArgs : EventArgs
 {
     public AimDirection aimDirection;
-    public float aimAngle;
+    public float aimAnlge;
     public float weaponAimAngle;
     public Vector3 weaponAimDirectionVector;
 }
-
-//16:00
